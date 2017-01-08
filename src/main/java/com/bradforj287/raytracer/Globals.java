9add8957ccd4f4e4c60ace1e24eccd5531ffba19
Brad Jones <brjones@akamai.com>
2017-01-08 10:07:56 -0500

    refactor

--- src/main/java/com/bradforj287/raytracer/Globals.java
+++ src/main/java/com/bradforj287/raytracer/Globals.java
@@ -2,16 +2,12 @@ package com.bradforj287.raytracer;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import com.google.common.eventbus.AsyncEventBus;
-import com.google.common.eventbus.EventBus;
 
 public class Globals {
     public static int NUM_THREADS = Runtime.getRuntime().availableProcessors();
     public static ExecutorService executorService;
-    public static EventBus rayTraceEventBus;
 
     static {
         executorService = Executors.newFixedThreadPool(NUM_THREADS);
-        rayTraceEventBus =  new AsyncEventBus(executorService);
     }
 }
